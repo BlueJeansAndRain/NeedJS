0917239f1a35eee8910d607d722f0ebe9bd7f401
BlueJeansAndRain <BlueJeansAndRain@gmail.com>
2013-09-27 00:20:19 -0700

    Added npm test

---
+++ test.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var Static = require("node-static").Server;
+var http = require('http');
+var path = require('path');
+
+var port = parseInt(process.env.npm_package_config_port, 10) || 8080;
+var docroot = new Static(path.join(__dirname, 'test'));
+
+http.createServer(function(req, res)
+{
+	req.on('end', function()
+	{
+		docroot.serve(req, res);
+	}).resume();
+}).listen(port);
+
+console.log("Listening on port " + port);
+console.log("Press Ctrl+C to quit");
+
+process
+	.on('SIGTERM', function()
+	{
+		process.exit();
+	})
+	.on('SIGINT', function()
+	{
+		process.exit();
+	})
+	.on('exit', function()
+	{
+		console.log("\rDone.");
+	});
